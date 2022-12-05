90e43c76f6c02b00582fa34fd1b7940dd9c65557
Dalrae03 <www298@naver.com>
2022-12-06 03:47:41 +0900

    운영하고있는 가게 사진리스트 만들고 랜덤 추출해서 가게이름 최소주문금액 배달비 평점 프린트 구현

--- code/DeliverPage/delivercrawling.py
+++ code/DeliverPage/delivercrawling.py
@@ -11,113 +11,58 @@ import json
 import pandas as pd
 
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-browser = webdriver.Chrome(options=options)
-
 browser = webdriver.Chrome()
 browser.get("https://www.yogiyo.co.kr/mobile/#/")
-# 요기요 검색창으로이동
+# 요기요로 이동
 
 
-elem = browser.find_element("name", "address_input")
-elem.clear()
-time.sleep(1)
-que = "세종특별자치시 조치원읍 신안리 300 홍익대학교세종캠퍼스" #검색어 입력
+elem = browser.find_element("name", "address_input")  #요기요 검색창 찾기
+elem.clear()  #기본적으로 입력되는것 지워주기
+time.sleep(1) #종종 1초 쉬어주기
+que = "세종특별자치시 조치원읍 신안리 300 홍익대학교세종캠퍼스"  #검색어 입력
 elem.send_keys(que)
 elem.send_keys(Keys.ENTER)
 time.sleep(1)
-#입력까진 완료인데 버튼이 안눌린다... 대가리 터질것같다...
-# browser.find_element("xpath", "//*[@id='search']/div/form/ul[2]/li[3]/a").click() <- 오류남
-browser.find_element("xpath", '//*[@id="search"]/div/form/ul/li[3]/a').click()
+browser.find_element("xpath", '//*[@id="search"]/div/form/ul/li[3]/a').click() #검색후 엔터한번 치고 검색창 밑에 뜨는 똑같은 주소 클릭하기
 time.sleep(1)
 
-List = browser.find_element("xpath", '//*[@id="content"]/div/div[4]/div/div[2]').find_elements("class name", "col-sm-6")
+List = browser.find_element("xpath", '//*[@id="content"]/div/div[4]/div/div[2]').find_elements("class name", "col-sm-6") 
+#요기요 등록 음식점에 있는 음식점들(정보상자) 리스트 만들기
 
-
-
-
-
-# for li in list:
-#   print(li.find_element("class name", "item clearfix").text)
-
-
-
-
-# print(browser.find_element("xpath", '//*[@id="content"]/div/div[2]/div/div[2]/div[2]/div')) -> 프린트는 되는 것 같다
-
-# browser.find_element("class name", "btn btn-default ico-pick").click()
-# time.sleep(2)
-
-# browser.find_element("class name", "form-control ng-pristine ng-valid ng-scope ng-valid-required ng-valid-minlength ng-touched").click()
-
-# browser.find_elements("class name", "restaurants-info")
-
-# print(url)
-
-'''
 Menu = []
-Menu.append(browser.find_elements("class name", "item clearfix"))  #가게 박스를 리스트로 넣어버림
-print(Menu)
-'''
-# pick = random.choice(Menu)
-# print(pick)
 
+for li in List:
+  if(li.find_element("tag name", "td").find_element("tag name", "div").get_attribute("class") == "logo"):  #리스트 전부 돌며 현재 영업안하는 음식점 걸러내기
+    Menu.append(li.find_element("tag name", "td").find_element("tag name", "div").get_attribute("style"))  #음식점들 사진 리스트 만들기
 
-'''
-res = requests.get(browser)  #브라우저 링크 수정해야함
-#우리지역에 해당하는 가게 모음 요기요링크
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")  #BeautifulSoup 객체 만들기
-'''
+  else:
+    pass
+
+# print(Menu)
+
+ch = random.choice(Menu)  #안의 사진주소들 모두 문자열 취급 / 사진리스트 중 하나 랜덤뽑기
+# print(ch)
+
+for li in List:  #뽑은 사진과 동일한 요소를 가지고있는 가게 정보상자 찾기
+  if(li.find_element("tag name", "td").find_element("tag name", "div").get_attribute("style") == ch): 
+    Site = li.find_element("tag name", "div")
+
+  else:
+    pass
+
+Site.click()  #정보 상자 클릭하기
+time.sleep(3)
 
 
 # 가게이름 최소주문금액 배달비 평점 <- 뽑아야할 것
-# 랜덤으로 고른 가게의 이름과 동일한 요소의 정보들을 뽑아야함 <- 수정요함
-'''
-  #가게이름
-  #최소주문금액
-#배달비
- #평점
-'''
+# 랜덤으로 고른 가게의 이름과 동일한 요소의 정보들을 뽑아야함
 
+Info = browser.find_element("class name", "col-sm-8").find_element("class name", "restaurant-info")
+Dliever = browser.find_element("tag name", "ng-include").find_element("tag name", "div")
 
-# 저 마지막이 끝나고나서 창이 사라저 버림 -> 함수가 종료되고 나서 크롬드라이버는 사라져버림 작동은 된다
-# 네이버 로그인 셀레니움
-'''
-browser.get("https://www.naver.com/")
-elem =browser.find_element("class name", "link_login") 
-elem.click()
+print(Info.find_element("class name", "restaurant-title").find_element("tag name", "span").text)  #가게이름 
+print(Info.find_element("class name", "restaurant-content").find_element("tag name", "ul").find_element("xpath", '//*[@id="content"]/div[2]/div[1]/div[1]/div[2]/ul/li[3]/span').text)
+#최소 주문금액
+print(Dliever.find_element("class name", "cart").find_element("class name", "clearfix").find_element("tag name", "span").text)  #배달비
+print(Info.find_element("class name", "restaurant-content").find_element("tag name", "ul").find_element("tag name", "span").text)  #평점
 
-browser.find_element("id", "id").send_keys("www298")
-browser.find_element("id", "pw").send_keys("yejin1020")
-
-browser.find_element("class name", "btn_login").click()
-'''
-
-
-
-
-
-# inform = browser.find_elements("class", "restaurants-info") 셀레니움 말고 수프 써야하는거 아님?
-
-
-'''
-res = requests.get(browser)
-#우리지역에 해당하는 가게 모음 요기요링크
-res.raise_for_status()
-
-soup = BeautifulSoup(res.text, "lxml")  #BeautifulSoup 객체 만들기
-
-Menu = []
-
-
-stname = soup.find_all("div", attrs = {"class":"restaurants-info"})  #여기서부터 싹다 수정하기...
-print('식당 수:', len(stname))
-
-
-for i, stname in enumerate(stname):
-    if i == 15:
-        break
-    print(stname.get_text())
-'''
